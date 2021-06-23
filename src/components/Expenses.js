a8f948ef20a1b9664b3f5468d66277add7df3ff3
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2021-06-23 09:53:12 +0530

    🎉

---
+++ src/components/Expenses.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "./UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.items.map((item) => {
+        return (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        );
+      })}
+    </Card>
+  );
+}
+
+export default Expenses;
